c8b4cb3ab398a7f01ce2aedd693614469b7259d4
jason <jason@gmail.com>
2022-04-28 17:52:39 +0800

    第二天

--- src/io/ReadStringDemo.java
+++ src/io/ReadStringDemo.java
@@ -14,10 +14,9 @@ public class ReadStringDemo {
         File file = new File("demo.txt");
         //将demo.txt文件中所有数据读取回来
         FileInputStream fis = new FileInputStream(file);
-
         long len = file.length();//获取文件的大小，单位字节
-        byte[] data = new byte[(int)len];
-        fis.read(data);
+        byte[] data = new byte[(int)len];//常见与文件大小一致的字节数组
+        fis.read(data);//块读，将文件所有字节读到data数组中
         /*
             Standard 标准
             Charsets 字符集
