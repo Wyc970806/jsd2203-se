46ccffb1efb790acac8dd4f8f750c6a47c6960aa
jason <jason@gmail.com>
2022-04-28 17:55:49 +0800

    第二天

---
+++ src/homework/day02/Test1.java
@@ -0,0 +1,12 @@
+package homework.day02;
+
+/**
+ * 实现一个记事本工具
+ * 程序启动后要求将用户在控制台上输入的每一行字符串都写入到文件note.txt中
+ * 如果用户在控制台单独输入了"exit"时，程序退出
+ */
+public class Test1 {
+    public static void main(String[] args) {
+
+    }
+}
