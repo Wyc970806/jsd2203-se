e27aff667fd534e9925e30bf0cbb920bed589915
jason <jason@gmail.com>
2022-05-05 10:31:50 +0800

    第四天:缓冲字符输出流

--- src/io/PWDemo2.java
+++ src/io/PWDemo2.java
@@ -1,6 +1,8 @@
 package io;
 
 import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
 
 /**
  * 在流连接中使用PrintWriter
@@ -9,12 +11,27 @@ public class PWDemo2 {
     public static void main(String[] args) throws FileNotFoundException {
         //如果希望有追加模式，则需要自行创建文件输出流并指定
         FileOutputStream fos = new FileOutputStream("pw.txt",true);
-        OutputStreamWriter osw = new OutputStreamWriter(fos);
+        //在转换流上加上字符集，可以按照指定字符集写出。
+        OutputStreamWriter osw = new OutputStreamWriter(fos, StandardCharsets.UTF_8);
         BufferedWriter bw = new BufferedWriter(osw);
-        PrintWriter pw = new PrintWriter(bw);
-        pw.println("让我掉下眼泪的，不止昨夜的酒。");
-        pw.println("让我依依不舍的，不止你的温柔。");
+        PrintWriter pw = new PrintWriter(bw,true);
 
+//        FileOutputStream fos = new FileOutputStream("pw.txt",true);
+//        BufferedOutputStream bw = new BufferedOutputStream(osw);
+        /*
+            不要将PW直接链接在缓冲字节输出流上，因为PW内部总是链接这BW这个缓冲字符输出流了。
+            不需要在缓冲字节流上再缓冲了，会降低效率
+         */
+//        PrintWriter pw = new PrintWriter(bw);
+
+        Scanner scanner = new Scanner(System.in);
+        while(true) {
+            String line = scanner.nextLine();
+            if("exit".equalsIgnoreCase(line)){
+                break;
+            }
+            pw.println(line);
+        }
         System.out.println("写出完毕!");
 
         pw.close();
