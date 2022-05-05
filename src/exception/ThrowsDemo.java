7929f93224ad5b0d58ba436beec8cd076fd95c1d
jason <jason@gmail.com>
2022-05-05 17:03:05 +0800

    第四天:异常处理机制

---
+++ src/exception/ThrowsDemo.java
@@ -0,0 +1,31 @@
+package exception;
+
+import java.awt.*;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+/**
+ * 子类在重写超类中含有throws声明异常抛出的方法时对throws的重写规则
+ */
+public class ThrowsDemo {
+    public void dosome()throws IOException, AWTException {}
+}
+
+class SubClass extends ThrowsDemo{
+//    public void dosome()throws IOException, AWTException {}
+
+    //允许子类方法不再抛出任何异常
+//    public void dosome(){}
+
+    //允许子类方法抛出部分异常
+//    public void dosome()throws IOException{}
+
+    //允许子类方法抛出超类方法声明异常的子类型异常
+//    public void dosome()throws FileNotFoundException {}
+}
+
+
+
+
+
+
