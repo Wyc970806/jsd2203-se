961dcfe7e59f36c7492a74daef94ee5242f3be5b
jason <jason@gmail.com>
2022-05-05 11:55:46 +0800

    第四天:异常处理机制

---
+++ src/exception/TryCatchDemo.java
@@ -0,0 +1,13 @@
+package exception;
+
+/**
+ * 异常处理机制中的try-catch
+ * 语法:
+ * try{
+ *     可能出现异常的代码片段
+ * }catch(XXXException e){
+ *     try语句块中出现XXXException后的处理代码
+ * }
+ */
+public class TryCatchDemo {
+}
