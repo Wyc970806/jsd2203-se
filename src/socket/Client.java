6970c9389a49e411e1a11363354efd82a17fdbb4
Xiloer <83123095@qq.com>
2022-05-06 11:00:22 +0800

    第五天:聊天室

---
+++ src/socket/Client.java
@@ -0,0 +1,30 @@
+package socket;
+
+import java.net.Socket;
+
+/**
+ * 聊天室的客户端
+ */
+public class Client {
+    private Socket socket;
+
+    public Client(){
+
+    }
+
+    public void start(){
+
+    }
+
+    public static void main(String[] args) {
+        Client client = new Client();
+        client.start();
+    }
+}
+
+
+
+
+
+
+
