3e208854f0750a70b4cae1c4567a0287508ff5da
Xiloer <83123095@qq.com>
2022-05-06 11:09:34 +0800

    第五天:聊天室

--- src/socket/Client.java
+++ src/socket/Client.java
@@ -1,15 +1,39 @@
 package socket;
 
+import java.io.IOException;
 import java.net.Socket;
 
 /**
  * 聊天室的客户端
  */
 public class Client {
+    /*
+        java.net.Socket
+        封装了TCP协议的通讯细节，我们基于它就可以与远端计算机建立TCP连接，并基于一对
+        输入与输出流的读写完成与远端计算机的数据交互操作。
+        可以把Socket想象成"电话"
+     */
     private Socket socket;
-
+    /*
+        构造器中完成初始化操作
+     */
     public Client(){
+        try {
+            /*
+                Socket的构造器:
+                Socket(String host,int port)
+                参数1:要连接的远端计算机(服务器)的IP地址
+                参数2:要连接的运行在远端计算机上的服务端应用程序开启的服务端口
+                我们通过IP地址可以找到网络上的服务器计算机，通过端口可以连接到运行在
+                该服务器计算上的服务端应用程序。
 
+                并且这个构造器实例化Socket的过程就是与服务端建立连接的过程，如果连接
+                失败会抛出异常.
+             */
+            socket = new Socket("",0);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void start(){
