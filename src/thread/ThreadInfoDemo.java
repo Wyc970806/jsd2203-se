a49fb39a00b3feee1e512d9040374dbc69d98ee6
jason <jason@gmail.com>
2022-05-07 16:29:43 +0800

    第六天:多线程

---
+++ src/thread/ThreadInfoDemo.java
@@ -0,0 +1,20 @@
+package thread;
+
+/**
+ * Thread提供了一套获取线程相关信息的方法(了解即可)
+ */
+public class ThreadInfoDemo {
+    public static void main(String[] args) {
+        Thread main = Thread.currentThread();//获取主线程
+
+
+    }
+}
+
+
+
+
+
+
+
+
