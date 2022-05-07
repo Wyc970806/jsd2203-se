79b37e43fd3427fd77ff56c526b9954f33fe803e
jason <jason@gmail.com>
2022-05-07 20:20:01 +0800

    第六天:多线程

--- src/thread/SleepDemo.java
+++ src/thread/SleepDemo.java
@@ -9,7 +9,10 @@ package thread;
 public class SleepDemo {
     public static void main(String[] args) {
         System.out.println("程序开始了...");
-
+        /*
+            倒计时程序  github.com
+            程序启动后要求在控制台上输入一个数字，然后从该数字开始每秒递减。到0时输出时间到。
+         */
         try {
             Thread.sleep(5000);
         } catch (InterruptedException e) {
