b0f94d00c2c55da5a600519ee8d21acd5c4f1692
jason <jason@gmail.com>
2022-05-07 14:03:50 +0800

    第六天:多线程

--- src/thread/ThreadDemo3.java
+++ src/thread/ThreadDemo3.java
@@ -14,14 +14,20 @@ public class ThreadDemo3 {
             }
         };
         //第二种创建方式:实现Runnable接口重写run方法
-        Runnable r2 = new Runnable(){
-            public void run(){
-                for(int i=0;i<1000;i++){
-                    System.out.println("我是查水表的!");
-                }
+//        Runnable r2 = new Runnable(){
+//            public void run(){
+//                for(int i=0;i<1000;i++){
+//                    System.out.println("我是查水表的!");
+//                }
+//            }
+//        };
+//        Thread t2 = new Thread(r2);
+
+        Thread t2 = new Thread(()->{
+            for(int i=0;i<1000;i++){
+                System.out.println("我是查水表的!");
             }
-        };
-        Thread t2 = new Thread(r2);
+        });
 
         t1.start();
         t2.start();
