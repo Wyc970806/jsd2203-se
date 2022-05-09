dccf45759baf134758e0472c6b49769823f4c15a
jason <jason@gmail.com>
2022-05-09 09:15:11 +0800

    第六天:多线程

--- src/thread/SleepDemo.java
+++ src/thread/SleepDemo.java
@@ -1,5 +1,7 @@
 package thread;
 
+import java.util.Scanner;
+
 /**
  * 睡眠阻塞
  * Thread提供了一个静态方法:
@@ -13,12 +15,17 @@ public class SleepDemo {
             倒计时程序  github.com
             程序启动后要求在控制台上输入一个数字，然后从该数字开始每秒递减。到0时输出时间到。
          */
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("请输入一个数字:");
+        for(int num = scanner.nextInt();num>0;num--){//number数字
+            System.out.println(num);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
-
+        System.out.println("时间到");
         System.out.println("程序结束了!");
     }
 }
