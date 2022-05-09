ab763adb0f0de177015e49b952a2f80b9eacf51a
jason <jason@gmail.com>
2022-05-09 17:22:24 +0800

    第七天:多线程

--- src/thread/SyncDemo4.java
+++ src/thread/SyncDemo4.java
@@ -16,25 +16,31 @@ public class SyncDemo4 {
 }
 
 class Boo{
-    public synchronized void methodA(){
-        Thread t = Thread.currentThread();
-        System.out.println(t.getName()+":正在执行A方法...");
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+    public void methodA(){
+        synchronized(Boo.class) {
+            Thread t = Thread.currentThread();
+            System.out.println(t.getName() + ":正在执行A方法...");
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(t.getName() + ":执行A方法完毕!");
         }
-        System.out.println(t.getName()+":执行A方法完毕!");
     }
-    public synchronized void methodB(){
-        Thread t = Thread.currentThread();
-        System.out.println(t.getName()+":正在执行B方法...");
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+
+    //    public synchronized void methodB(){
+    public void methodB(){
+        synchronized (Boo.class) {
+            Thread t = Thread.currentThread();
+            System.out.println(t.getName() + ":正在执行B方法...");
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(t.getName() + ":执行B方法完毕!");
         }
-        System.out.println(t.getName()+":执行B方法完毕!");
     }
 }
 
