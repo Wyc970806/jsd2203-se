e0c0fd3a50fc507f572c54e7293d6d5841c1a612
jason <jason@gmail.com>
2022-05-09 18:21:59 +0800

    第七天:多线程

---
+++ src/homework/day07/Test05.java
@@ -0,0 +1,26 @@
+package homework.day07;
+
+/**
+ * 修改下面代码的编译
+ *
+ * @author Xiloer
+ */
+public class Test05 {
+    public static void main(String[] args) {
+//		Thread t1 = new Thread() {
+//			Foo.dosome();
+//		};
+//		Thread t2 = new Thread() {
+//			Foo.dosome();
+//		};
+//		t1.start();
+//		t2.start();
+    }
+}
+class Foo{
+	public static void dosome() {
+//		synchronized (this) {
+//			System.out.println("hello!");
+//		}
+	}
+}
