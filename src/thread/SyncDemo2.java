50a946270b9c16a98d0770fc4ade746c039f6f5f
jason <jason@gmail.com>
2022-05-09 14:43:48 +0800

    第七天:多线程

--- src/thread/SyncDemo2.java
+++ src/thread/SyncDemo2.java
@@ -5,7 +5,7 @@ package thread;
  * 有效的缩小同步范围可以在保证并发安全的前提下尽可能提高并发效率。
  *
  * 语法:
- * synchronized(同步建设机器对象){
+ * synchronized(同步监视器对象){
  *     需要同步执行的代码片段
  * }
  */
@@ -22,21 +22,28 @@ public class SyncDemo2 {
                 shop.buy();
             }
         };
-
         t1.start();
         t2.start();
     }
 }
 
 class Shop{
-    public synchronized void buy(){
+//    public synchronized void buy(){
+    public void buy(){
         try {
             Thread t = Thread.currentThread();//获取执行buy方法的线程
             System.out.println(t.getName()+":正在挑衣服...");
             Thread.sleep(5000);
-
-            System.out.println(t.getName()+":正在试衣服...");
-            Thread.sleep(5000);
+            /*
+                同步块可以更精准的控制需要多个线程同步执行的代码片段，这里需要在"()"
+                中指定同步监视器对象，该对象可以是任何引用类型(语法层面上)，但是要想
+                让多个线程同步执行该同步块，必须保证这些线程看到的这个同步监视器对象
+                是【同一个】才行!
+             */
+            synchronized (this) {
+                System.out.println(t.getName() + ":正在试衣服...");
+                Thread.sleep(5000);
+            }
 
             System.out.println(t.getName()+":结账离开");
         } catch (InterruptedException e) {
