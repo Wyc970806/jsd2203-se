b0ee2268b00d6969f95963a900321eee221fd89e
jason <jason@gmail.com>
2022-05-09 14:26:47 +0800

    第七天:多线程

--- src/thread/SyncDemo2.java
+++ src/thread/SyncDemo2.java
@@ -11,6 +11,40 @@ package thread;
  */
 public class SyncDemo2 {
     public static void main(String[] args) {
+        Shop shop = new Shop();
+        Thread t1 = new Thread("范传奇"){
+            public void run(){
+                shop.buy();
+            }
+        };
+        Thread t2 = new Thread("王克晶"){
+            public void run(){
+                shop.buy();
+            }
+        };
 
+        t1.start();
+        t2.start();
     }
 }
+
+class Shop{
+    public synchronized void buy(){
+        try {
+            Thread t = Thread.currentThread();//获取执行buy方法的线程
+            System.out.println(t.getName()+":正在挑衣服...");
+            Thread.sleep(5000);
+
+            System.out.println(t.getName()+":正在试衣服...");
+            Thread.sleep(5000);
+
+            System.out.println(t.getName()+":结账离开");
+        } catch (InterruptedException e) {
+        }
+    }
+}
+
+
+
+
+
