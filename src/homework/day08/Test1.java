331fa115523b3a7e6595a90e0f63bc3fa64ae2e7
Xiloer <83123095@qq.com>
2022-05-10 20:06:27 +0800

    第八天:笔记

--- src/homework/day08/Test1.java
+++ src/homework/day08/Test1.java
@@ -11,10 +11,10 @@ public class Test1 {
 	public static void main(String[] args) {
 		int a = 1;
 		String str = "hello";
-		day09.Point p = new day09.Point(1,2);
+		Point p = new Point(1,2);
 		String[] arr = {"one","two","three",str};
 		int[] arr1 = {1,2,3,4,a};
-		day09.Point[] arr2 = {new day09.Point(1,2),new day09.Point(3,4),new day09.Point(5,6),p};
+		Point[] arr2 = {new Point(1,2),new Point(3,4),new Point(5,6),p};
 		dosome(a,str,p,arr,arr1,arr2);
 		System.out.println("a:"+a);//1
 		System.out.println("str:"+str);//hello
@@ -24,7 +24,7 @@ public class Test1 {
 		System.out.println("arr2:"+Arrays.toString(arr2));//[(1,2), (3,4), (12,6), (4,2)]
 	}
 	
-	public static void dosome(int a, String str, day09.Point p, String[] arr, int[] arr1, day09.Point[] arr2) {
+	public static void dosome(int a, String str, Point p, String[] arr, int[] arr1, Point[] arr2) {
 		arr1[a] = 3;
 		arr[a] = "str";
 		arr[a+1] = str;
@@ -36,7 +36,7 @@ public class Test1 {
 		p.setX(6);
 		arr2 = Arrays.copyOf(arr2, arr2.length-1);
 		arr2[2].setX(8);
-		arr2 = new day09.Point[]{new day09.Point(2,2),p};
+		arr2 = new Point[]{new Point(2,2),p};
 		arr2[arr2.length-1].setX(12);
 	}
 }
