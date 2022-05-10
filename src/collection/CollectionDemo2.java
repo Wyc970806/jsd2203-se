b071d889cfb9b75af1e60251df253e94bd0cbeb7
jason <jason@gmail.com>
2022-05-10 17:12:39 +0800

    第八天:集合

--- src/collection/CollectionDemo2.java
+++ src/collection/CollectionDemo2.java
@@ -14,6 +14,7 @@ public class CollectionDemo2 {
         c.add(new Point(4,5));
         c.add(new Point(6,7));
         c.add(new Point(8,9));
+        c.add(new Point(1,2));
         /*
             集合重写了toString方法，格式为:
             [元素1.toString(), 元素2.toString(), ......]
@@ -29,6 +30,13 @@ public class CollectionDemo2 {
          */
         boolean contains = c.contains(p);
         System.out.println("集合是否包含该元素:"+contains);
+
+        /*
+            remove方法删除元素时也是删除与集合元素equals比较为true的元素
+            注意:对于List集合而言，重复元素仅会删除一次。
+         */
+        c.remove(p);
+        System.out.println(c);
 
 
 
