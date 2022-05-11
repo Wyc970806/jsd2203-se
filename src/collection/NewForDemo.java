5769f3a8101eef5f5ef9ef81fa54ad94f89221ca
jason <jason@gmail.com>
2022-05-11 11:29:44 +0800

    第九天:集合

--- src/collection/NewForDemo.java
+++ src/collection/NewForDemo.java
@@ -2,6 +2,7 @@ package collection;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
 
 /**
  * JDK5之后推出了一个新的特性:增强型for循环
@@ -13,6 +14,14 @@ import java.util.Collection;
  * }
  *
  * 注意:新循环的语法是编译器认可的而非虚拟机
+ *
+ *
+ * JDK5推出的另一个特性:泛型
+ * 泛型也称为参数化类型，允许我们在使用一个类时通过泛型指定一个类中某个属性，或方法的参数，
+ * 或方法返回值的类型。使得我们使用更灵活。
+ * 泛型在集合中被广泛使用，用来指定集合中的元素类型。
+ *
+ *
  */
 public class NewForDemo {
     public static void main(String[] args) {
@@ -25,18 +34,29 @@ public class NewForDemo {
         for(String str : array){
             System.out.println(str);
         }
-
-        Collection c = new ArrayList();
-        c.add("一");
+        /*
+            泛型只能定义为引用类型。对于基本类型而言必须指定为其对应的包装类
+         */
+        Collection<String> c = new ArrayList<>();
+        c.add("一");//add(E e)，编译器会当E为String类型检查实参
         c.add("二");
         c.add("三");
         c.add("四");
         c.add("五");
-        for(Object o : c){
-            String str = (String)o;
+//        c.add(123);//实参不符合泛型要求时编译不通过。
+
+        /*
+            新循环遍历会被编译器改回成迭代器遍历
+         */
+        for(String str : c){
             System.out.println(str);
         }
 
+        Iterator<String> it = c.iterator();
+        while(it.hasNext()){
+            String str = it.next();
+            System.out.println(str);
+        }
     }
 }
 
