d7b0d9fa34cd3bf96b8c3ba4cf3c1806dab2e54a
jason <jason@gmail.com>
2022-05-11 16:18:17 +0800

    第九天:集合

--- src/collection/ListDemo3.java
+++ src/collection/ListDemo3.java
@@ -19,6 +19,22 @@ public class ListDemo3 {
         List<Integer> subList = list.subList(3,8);
         System.out.println(subList);
 
+        //将子集每个元素扩大10倍
+        for(int i=0;i<subList.size();i++){
+            int num = subList.get(i);
+            num = num * 10;
+            subList.set(i,num);
+        }
+        System.out.println(subList);//[30,40,50,60,70]
+        /*
+            对子集的任何操作就是对原集合对应元素的操作
+         */
+        System.out.println(list);//[0, 1, 2, 30, 40, 50, 60, 70, 8, 9]
+
+        //将list集合中2-8这部分元素删除
+        list.subList(2,9).clear();
+        System.out.println(list);
+
     }
 }
 
