25b3e1deac3841d23ee9bb1eab05d7cf6cbdaed4
jason <jason@gmail.com>
2022-05-12 10:55:55 +0800

    第十天:集合排序

--- src/collection/SortListDemo2.java
+++ src/collection/SortListDemo2.java
@@ -30,30 +30,45 @@ public class SortListDemo2 {
          */
 //        Collections.sort(list);
         //Type 类型
-        Comparator<Point> c = new Comparator<Point>() {
-            /**
-                重写compare方法的目的是定义o1与o2的大小关系。
-                返回值的定义:
-                当返回值>0时，表达的是o1>02
-                当返回值<0时，表达的是o1<o2
-                当返回值=0时，表达的是o1=o2
-             */
-            public int compare(Point o1, Point o2) {
-                //点到原点的距离长的大
-                int len1 = o1.getX()*o1.getX()+o1.getY()*o1.getY();
-                int len2 = o2.getX()*o2.getX()+o2.getY()*o2.getY();
-                return len1-len2;
-            }
-        };
-        /*
-            java有23种设计模式
-            回调模式
-            当我们定义一个类，并定义了对应的一些方法，但是这些方法我们并不会主动调用
-            而是传递给了一个功能，该功能内部实现过程中会反过来调用我们定义的这些方法，
-            这个过程称为:回调
-         */
-        Collections.sort(list,c);
+//        Comparator<Point> c = new Comparator<Point>() {
+//            /**
+//                重写compare方法的目的是定义o1与o2的大小关系。
+//                返回值的定义:
+//                当返回值>0时，表达的是o1>02
+//                当返回值<0时，表达的是o1<o2
+//                当返回值=0时，表达的是o1=o2
+//             */
+//            public int compare(Point o1, Point o2) {
+//                //点到原点的距离长的大
+//                int len1 = o1.getX()*o1.getX()+o1.getY()*o1.getY();
+//                int len2 = o2.getX()*o2.getX()+o2.getY()*o2.getY();
+////                return len1-len2;
+//                return len2-len1;//降序 两个数反过来相减就是降序
+//            }
+//        };
+//        /*
+//            java有23种设计模式
+//            回调模式
+//            当我们定义一个类，并定义了对应的一些方法，但是这些方法我们并不会主动调用
+//            而是传递给了一个功能，该功能内部实现过程中会反过来调用我们定义的这些方法，
+//            这个过程称为:回调
+//         */
+//        Collections.sort(list,c);
 
+
+        //没有侵入性写法
+//        Collections.sort(list,new Comparator<Point>() {
+//            public int compare(Point o1, Point o2) {
+//                int len1 = o1.getX()*o1.getX()+o1.getY()*o1.getY();
+//                int len2 = o2.getX()*o2.getX()+o2.getY()*o2.getY();
+//                return len1-len2;
+//            }
+//        });
+
+        Collections.sort(list,(o1,o2)->
+                o1.getX()*o1.getX()+o1.getY()*o1.getY() -
+                o2.getX()*o2.getX()-o2.getY()*o2.getY()
+        );
 
         System.out.println(list);
 
