f0baed2694cf4e0b6ad3a52d71620e381c06e7c4
jason <jason@gmail.com>
2022-05-12 10:35:44 +0800

    第十天:集合排序

--- src/collection/SortListDemo2.java
+++ src/collection/SortListDemo2.java
@@ -1,6 +1,8 @@
 package collection;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 /**
@@ -8,6 +10,7 @@ import java.util.List;
  */
 public class SortListDemo2 {
     public static void main(String[] args) {
+        //Element 元素
         List<Point> list = new ArrayList<>();
         list.add(new Point(5,6));
         list.add(new Point(15,12));
@@ -16,6 +19,44 @@ public class SortListDemo2 {
         list.add(new Point(3,4));
         list.add(new Point(9,8));
         System.out.println(list);
+        /*
+            compare:比较
+            comparable:可比较
+            sort方法在排序List集合时，要求集合元素必须实现了Comparable接口
+
+            这里该方式会对我们的代码造成侵入性:
+            当我们使用某一个功能时，其要求我们为其修改更多额外代码，这就是侵入性。
+            它不利于后期程序的维护。尽量避免。
+         */
+//        Collections.sort(list);
+        //Type 类型
+        Comparator<Point> c = new Comparator<Point>() {
+            /**
+                重写compare方法的目的是定义o1与o2的大小关系。
+                返回值的定义:
+                当返回值>0时，表达的是o1>02
+                当返回值<0时，表达的是o1<o2
+                当返回值=0时，表达的是o1=o2
+             */
+            public int compare(Point o1, Point o2) {
+                //点到原点的距离长的大
+                int len1 = o1.getX()*o1.getX()+o1.getY()*o1.getY();
+                int len2 = o2.getX()*o2.getX()+o2.getY()*o2.getY();
+                return len1-len2;
+            }
+        };
+        /*
+            java有23种设计模式
+            回调模式
+            当我们定义一个类，并定义了对应的一些方法，但是这些方法我们并不会主动调用
+            而是传递给了一个功能，该功能内部实现过程中会反过来调用我们定义的这些方法，
+            这个过程称为:回调
+         */
+        Collections.sort(list,c);
+
+
+        System.out.println(list);
+
 
     }
 }
