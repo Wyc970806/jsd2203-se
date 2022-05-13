0b6cff997e50210ce03cf78f9e6fad0243ee0b68
Xiloer <83123095@qq.com>
2022-05-13 15:51:42 +0800

    第十一天

--- src/map/MapDemo2.java
+++ src/map/MapDemo2.java
@@ -31,5 +31,19 @@ public class MapDemo2 {
         for(String key : keySet){
             System.out.println("key:"+key);
         }
+
+        /*
+            遍历每一组键值对
+            Set entrySet()
+            将Map中每一组键值对以一个Entry实例表示，并将所有的Entry实例存入Set
+            集合后将其返回
+            Entry是Map的内部接口，每一个Entry实例应当表示Map中的一组键值对。
+         */
+        Set<Map.Entry<String,Integer>> entrySet = map.entrySet();
+        for(Map.Entry<String,Integer> entry: entrySet){
+            String key = entry.getKey();
+            Integer value = entry.getValue();
+            System.out.println(key+":"+value);
+        }
     }
 }
