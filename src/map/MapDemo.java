498aea69c1b996e06091f12a6215c9f6926b7bd4
Xiloer <83123095@qq.com>
2022-05-13 14:40:36 +0800

    第十一天

---
+++ src/map/MapDemo.java
@@ -0,0 +1,16 @@
+package map;
+
+/**
+ * Map 查找表
+ * java.util.Map接口，规定了Map的相关功能。
+ * Map体现的结构是一个多行两列的表格，其中左列称为key，右列称为value
+ * Map总是根据key获取value.
+ *
+ * 常用的实现类:
+ * java.util.HashMap散列表，当今查询速度最块的数据结构。
+ */
+public class MapDemo {
+    public static void main(String[] args) {
+
+    }
+}
