68eb4a37a13bd9693c716215e52f84f86000bb34
Xiloer <83123095@qq.com>
2022-05-23 09:26:22 +0800

    第十一天

--- src/reflect/ReflectDemo1.java
+++ src/reflect/ReflectDemo1.java
@@ -7,6 +7,36 @@ package reflect;
  */
 public class ReflectDemo1 {
     public static void main(String[] args) {
+       /*
+            反射机制第一步:在程序运行期间获取要操作的类的类对象。
+            类对象:Class类的实例
+            JVM加载一个类时，就会实例化一个Class的实例与其绑定，并且这个Class实例中会记录加载的类的一切信息:
+            类名，包名，属性，方法，构造器。
+
+            获取一个类的类对象的方式:
+            1:类名.class
+            比如:
+                Class cls = String.class;
+                Class cls = int.class;
+                注意:基本类型获取对应的类对象，只有这一种方式。
+
+            2:Class.forName(String className)
+            比如:
+                Class cls = Class.forName("java.lang.String")
+                注意:参数要求传入的是对应类的完全限定名:包名.类名
+
+            3:ClassLoader方式加载
+        */
+
 
     }
+
+
+
 }
+
+
+
+
+
+
