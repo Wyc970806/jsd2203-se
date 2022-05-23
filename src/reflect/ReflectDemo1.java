a8d049951f0eafd7442c6439c213b5ccd1623456
Xiloer <83123095@qq.com>
2022-05-23 09:13:51 +0800

    第十一天

---
+++ src/reflect/ReflectDemo1.java
@@ -0,0 +1,12 @@
+package reflect;
+
+/**
+ *  JAVA 反射机制
+ *  反射是java的动态机制，允许我们在程序[运行期间]再确定对象的实例化，属性的操作，方法的调用等。
+ *  反射机制可以提高代码的灵活度，但同时带来了更多的资源开销和较慢的运行效率，因此不能过度依赖。
+ */
+public class ReflectDemo1 {
+    public static void main(String[] args) {
+
+    }
+}
