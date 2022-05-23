908d1ecc50e692566f3fd15c36889a6f8d910475
Xiloer <83123095@qq.com>
2022-05-23 11:59:23 +0800

    第十一天

--- src/reflect/ReflectDemo4.java
+++ src/reflect/ReflectDemo4.java
@@ -1,6 +1,7 @@
 package reflect;
 
 import java.lang.reflect.Method;
+import java.util.Scanner;
 
 /**
  * 利用反射机制调用方法
@@ -18,9 +19,8 @@ public class ReflectDemo4 {
             4:利用方法对象进行方法的调用
          */
         //Person p2 = new Person();
-        Class cls = Class.forName("reflect.Person");
-        Object p2 = cls.newInstance();
-
+//        Class cls = Class.forName("reflect.Person");
+//        Object p2 = cls.newInstance();
         /*
             Class提供了获取其表示的类中某个特定的方法的操作:
             Method getMethod(String methodName,Class... argsType)
@@ -28,7 +28,7 @@ public class ReflectDemo4 {
             参数2:实参列表。当获取的方法没有参数时，这里可以省略。
          */
         //p2.sayHello();
-        Method method = cls.getMethod("sayHello");//通过类对象获取Person的成员方法sayHello()
+//        Method method = cls.getMethod("sayHello");//通过类对象获取Person的成员方法sayHello()
         /*
             Method对象提供了调用该方法的操作:
             Object invoke(Object obj,Object[] args)
@@ -36,7 +36,24 @@ public class ReflectDemo4 {
             参数2:该方法的实参列表
             返回值:实际调用该方法后该方法的返回值。
          */
-        method.invoke(p2);
+//        method.invoke(p2);
+
+
+
+        //动态操作:
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("请输入类名:");
+        String className = scanner.nextLine();
+        System.out.println("请输入方法名:");
+        String methodName = scanner.nextLine();
+
+        Class cls = Class.forName(className);
+        Object o = cls.newInstance();//实例化对象
+        Method method = cls.getMethod(methodName);
+        method.invoke(o);//调用该对象的对应方法
+
+
+
     }
 }
 
